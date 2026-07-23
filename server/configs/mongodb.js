c175d287c9f8c3fcea4ff34968a6a1b7f6a8fe37
Aditya Arya <adityaarya15283@gmail.com>
2026-07-23 17:25:54 +0530

    vite-change

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("mongoDb connected"));
+        mongoose.connection.on('error', (err) => console.log("mongoDb connection error:", err.message));
 
-    mongoose.connection.on('connected',()=> console.log("mongoDb connected"))
-    mongoose.connection.on('error',()=> console.log("mongoDb connection error"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error.message);
+    }
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
-    
-}
-export default connectDB
+export default connectDB;
